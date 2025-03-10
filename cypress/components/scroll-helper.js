eaebbf9cc4a3d94fe765fde42a693bfa285769f9
jeconiahb <97310693+jeconiahb@users.noreply.github.com>
2025-03-10 16:34:26 +0700

    Create scroll-helper.js

---
+++ cypress/components/scroll-helper.js
@@ -0,0 +1,27 @@
+import 'cypress-xpath';
+
+export class ScrollHelper {
+    /**
+     * Scroll until a certain element gets into view
+     * @param {string} element - The XPath selector of the element to scroll to
+     */
+    // static toView(element) {
+    //     cy.xpath(element)
+    //         .scrollIntoView({ block: 'center' }) // Ensure it's centered
+    //         .should('be.visible');
+    // }
+
+     static toView(element) {
+        cy.xpath(element).then(($el) => {
+            cy.window().then((win) => {
+                const rect = $el[0].getBoundingClientRect();
+                const scrollY = win.scrollY + rect.top - (win.innerHeight / 2);
+                win.scrollTo({ top: scrollY, behavior: "smooth" });
+            });
+        });
+
+        // Pastikan elemen terlihat setelah scroll
+        cy.xpath(element).should('be.visible');
+    }
+}
+
